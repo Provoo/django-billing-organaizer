3d43a8c5a7f11112c564caadd0819dfbc6c8fbca
Carlos V <design24439@gmail.com>
2017-04-12 23:35:56 -0500

    algoritmo de lectura de xml funcionando con factura y tagas del xml de favorita, donde se conecta a unbase para saber los tagas y se pueden modificar en función de cada una de las facturas

--- ProvooApp/DocumentReader/models.py
+++ ProvooApp/DocumentReader/models.py
@@ -9,23 +9,28 @@ class EnterprisesMetadataEcuador(models.Model):
     Country = models.CharField(max_length=40)
     NameDocument = models.CharField(max_length=40)
     IdDocument = models.CharField(max_length=40)
-    Parser_Flag = models.IntegerField()
     Type_Document_Flag = models.IntegerField()
     Clienteid = models.CharField(max_length=40)
     EnterpriseId = models.CharField(max_length=40)
     EnterpriseName = models.CharField(max_length=40)
     EnterpriseComercialName = models.CharField(max_length=40)
+    Secuential1 = models.CharField(max_length=40)
+    Secuential2 = models.CharField(max_length=40)
+    Secuential3 = models.CharField(max_length=40)
+    EnterpriseTaxPercent = models.DecimalField(max_digits=4, decimal_places=2)
     EnterpriseDateAuth = models.CharField(max_length=40)
     EnterpriseAddress = models.CharField(max_length=40)
     EnterpriseNoTax = models.CharField(max_length=40)
-    EnterpriseTotalTax = models.CharField(max_length=40)
-    EnterpriseTotalTax = models.CharField(max_length=40)
+    EnterpriseBaseTax = models.CharField(max_length=40)
     EnterpriseTotal = models.CharField(max_length=40)
-    EnterpriseFood = models.CharField(max_length=40)
+    EnterpriseFood = models.CharField(max_length=100)
     EnterpriseMed = models.CharField(max_length=40)
     EnterpriseClothes = models.CharField(max_length=40)
     EnterpriseEdu = models.CharField(max_length=40)
     EnterpriseHome = models.CharField(max_length=40)
+
+
+
 
     def __str__(self):
         return '%s' % (self.NameDocument)
--- ProvooApp/DocumentReader/xmlReader.py
+++ ProvooApp/DocumentReader/xmlReader.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
 import xml.etree.ElementTree as ET
 from DocumentReader.models import EnterprisesMetadataEcuador as MetaData
 from datetime import datetime
+from decimal import Decimal
 
 '''
-Código lector de facturas, es importante tomar en cuenta que algunas facturas
+Codigo lector de facturas, es importante tomar en cuenta que algunas facturas
 tienen mal definido el xml, es por esto que se lo transforma a texto nuevamente
 y se lo convierte en xml para que coincida con las tags definidas.
 
@@ -40,13 +43,14 @@ def get_xml_tree(xml_file):
 
 def readDocumentXML(xml_document):
     root = get_xml_tree(xml_document)
-    iterat = root.getiterator()
     document_object = {}
     search_ruc = ""
-    for child in tree.iter('ruc'):
+    for child in root.iter('ruc'):
         if child.text:
             search_ruc = child.text
 
+    Variables_Enterprise = MetaData.objects.get(IdDocument=search_ruc)
+    print("imprimiendo objeto de la base: %s " % (Variables_Enterprise))
     # Seteamos Diccionario con los valores por defecto
     document_object['NUMERO_DOCUMENTO'] = ''
     document_object['DEDUCIBLE_COMIDA'] = 0.00
@@ -55,25 +59,23 @@ def readDocumentXML(xml_document):
     document_object['DEDUCIBLE_SALUD'] = 0.00
     document_object['NO_DEDUCIBLE'] = 0.00
     document_object['ARCHIVO'] = xml_document
-    for child in iterat:
-        if child.tag == "ruc":
-            search_var = child.text
 
-    for child in iterat:
-            if child.tag == "identificacionComprador":
+    for child in root.iter():
+            if child.tag == Variables_Enterprise.Clienteid:
                 document_object['RUC_XML'] = child.text
 
-            if child.tag == "ruc":
+            if child.tag == Variables_Enterprise.EnterpriseId:
                 document_object['RUC_EMISOR'] = child.text
 
-            if child.tag == "nombreComercial":
+            if child.tag == Variables_Enterprise.EnterpriseComercialName:
                 document_object['NOMBRE_EMISOR'] = child.text
 
-            if child.tag == "dirEstablecimiento":
+            if child.tag == Variables_Enterprise.EnterpriseAddress:
                 document_object['DIRECCION_EMISOR'] = child.text
 
-            if child.tag == "fechaAutorizacion":
-                document_object['FECHA'] = datetime.strptime(child.text, "%d/%m/%Y %H:%M:%S")
+            if child.tag == Variables_Enterprise.EnterpriseDateAuth:
+                document_object['FECHA'] = datetime.strptime(
+                    child.text, "%d/%m/%Y")
 
             if child.tag == "estab":
                 document_object['NUMERO_DOCUMENTO'] = child.text
@@ -84,25 +86,23 @@ def readDocumentXML(xml_document):
             if child.tag == "secuencial":
                 document_object['NUMERO_DOCUMENTO'] += '-%s' % (child.text)
 
-            if child.tag == "totalSinImpuestos":
-                document_object['TOTAL_GASTOS'] = float(child.text)
+            if child.tag == Variables_Enterprise.EnterpriseBaseTax:
+                document_object['TOTAL_GASTOS'] = Decimal(child.text) * Variables_Enterprise.EnterpriseTaxPercent / 100
 
-            if child.tag == "totalConImpuestos":
-                imp = child.getiterator()
-                for nchild in imp:
-                    if nchild.tag == "valor":
-                        document_object['TOTAL_IMPUESTOS'] = float(nchild.text)
-                        print("{} =  {}".format(nchild.tag, nchild.text))
-                        break
-
-            if child.tag == "importeTotal":
+            if child.tag == Variables_Enterprise.EnterpriseTotal:
                 document_object['TOTAL_DOCUMENTO'] = float(child.text)
 
-            if child.tag == "campoAdicional" and \
-                    child.attrib.get('nombre') == "DEDUCIBLE ALIMENTACION":
-                document_object['DEDUCIBLE_COMIDA'] = float(child.text)
 
     # End For
+
+    # Busqueda de campos adicionales comida:
+    if Variables_Enterprise.EnterpriseFood != 'null':
+        additional = root.findall(Variables_Enterprise.EnterpriseFood)
+        if additional:
+            for a in additional:
+                document_object['DEDUCIBLE_COMIDA'] = float(a.text)
+                print("probando busqueda de atributo: %s" % (a.text))
+
     document_object['NO_DEDUCIBLE'] = document_object['TOTAL_DOCUMENTO'] -\
         document_object['DEDUCIBLE_COMIDA'] -\
         document_object['DEDUCIBLE_VESTIMENTA'] -\
@@ -110,3 +110,8 @@ def readDocumentXML(xml_document):
         document_object['DEDUCIBLE_SALUD']
 
     return document_object
+
+# import os
+# from DocumentReader.xmlReader import readDocumentXML
+# path = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'xml_de_prueba/favorita022017.xml')
+# readDocumentXML(path)
